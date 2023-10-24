41a816d2a45b5038fce5987b257067d466d69941
Azamat <agaifullin157@gmail.com>
2023-10-24 15:37:54 +0300

    hw05

---
+++ hw05/src/hw05.cpp
@@ -0,0 +1,75 @@
+//#include <iostream>
+#include <vector>
+#include <string> 
+using VecOfStr = std::vector<std::string>;
+
+
+VecOfStr hw05(const VecOfStr& strings) {
+    auto process_string = [](const std::string& str) -> std::string {
+        std::string result;
+        for (const auto& ch : str) {
+            if (std::isalnum(ch)) {
+                result += std::tolower(ch);
+            }
+        }
+        return result;
+    };
+
+    VecOfStr result;
+    for (const auto& str : strings) {
+        std::string processed_str = process_string(str);
+        bool is_palindrome = true;
+        auto left = processed_str.begin();
+        auto right = processed_str.rbegin();
+        while (left != processed_str.end() && right != processed_str.rend()) {
+            if (*left != *right) {
+                is_palindrome = false;
+                break;
+            }
+            ++left;
+            ++right;
+        }
+        if (is_palindrome) {
+            result.push_back(str);
+        }
+    }
+    return result;
+}
+// int main() {
+//     VecOfStr input = {"madam", "radar", "anagram", "rotor"};
+//     VecOfStr output = hw05(input);
+//     for (const auto& str : output) {
+//         std::cout << str << " ";
+//     }
+//     std::cout << std::endl;
+
+//     input = {"Madam, in Eden, I'm Adam"};
+//     output = hw05(input);
+//     for (const auto& str : output) {
+//         std::cout << str << " ";
+//     }
+//     std::cout << std::endl;
+
+//     input = {"A man, a plan, a canal, Panama!", "SIMPLETEXT"};
+//     output = hw05(input);
+//     for (const auto& str : output) {
+//         std::cout << str << " ";
+//     }
+//     std::cout << std::endl;
+
+//     input = {"Won't lovers revolt now?"};
+//     output = hw05(input);
+//     for (const auto& str : output) {
+//         std::cout << str << " ";
+//     }
+//     std::cout << std::endl;
+
+//     input = {"Red roses run no risk, sir, on Nurse's order"};
+//     output = hw05(input);
+//     for (const auto& str : output) {
+//         std::cout << str << " ";
+//     }
+//     std::cout << std::endl;
+
+//     return 0;
+// }
